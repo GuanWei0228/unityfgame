7530e437d58b8e9817802c441ea1b92e395f0a02
s1090349 <91927342+s1090349@users.noreply.github.com>
2023-11-06 03:02:12 +0800

    11/5 怪物 boss更新
    11/5 怪物 boss更新

---
+++ Assets/Scripts/Monster/BossMonsterSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMonsterSpawner : MonoBehaviour
+{
+    public GameObject[] monsterPrefabs; // 存储不同种类的怪物预制体
+    public Transform spawnPoint; // 生成怪物的位置
+    public int yourMaxValue = 1;
+    private RoomGenerator roomGenerator;
+
+    private void Start()
+    {
+        roomGenerator = FindObjectOfType<RoomGenerator>();
+
+        for (int i = 0; i < yourMaxValue; i++)
+        {
+            // 从怪物预制体数组中随机选择一种怪物
+            int randomMonsterIndex = Random.Range(0, monsterPrefabs.Length);
+            GameObject selectedMonsterPrefab = monsterPrefabs[randomMonsterIndex];
+
+            // 实例化怪物并放置在生成位置
+            GameObject spawnedBoss = Instantiate(selectedMonsterPrefab, spawnPoint.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Monster/EnemyMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed; // 敌人的移动速度
+    public float detectionRange; // 玩家探测范围
+    public float stoppingDistance; // 距离玩家多远时停止移动
+    public float followDelay; // 停留在探测范围内一定时间后开始跟随
+    public Transform playerTransform; // 玩家的Transform组件
+
+    private bool playerInDetectionRange; // 玩家是否在探测范围内
+    private float followTimer; // 跟随计时器
+    private bool shouldMove = true; // 是否应该移动
+    public Vector3 initialPosition; // 初始位置
+
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+        // 使用FindObjectOfType查找玩家对象
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerObject != null)
+        {
+            playerTransform = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogError("找不到玩家对象，请确保玩家对象具有“Player”标签。");
+        }
+
+    }
+
+    private void Update()
+    {
+        // 检测玩家是否在探测范围内
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+
+        if (shouldMove && distanceToPlayer <= detectionRange)
+        {
+            playerInDetectionRange = true;
+
+            // 开始计时
+            followTimer += Time.deltaTime;
+
+            // 如果跟随计时器达到设定时间
+            if (followTimer >= followDelay)
+            {
+                // 如果玩家在探测范围内，就朝向玩家移动
+                Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
+                transform.Translate(directionToPlayer * moveSpeed * Time.deltaTime);
+
+                // 如果距离玩家够近，停止移动
+                if (distanceToPlayer <= stoppingDistance)
+                {
+                    moveSpeed = 0;
+                    shouldMove = false;
+                    return;
+                }
+            }
+        }
+        else
+        {
+            // 如果玩家不在探测范围内，重置计时器和移动标志
+            moveSpeed = 2f;
+            playerInDetectionRange = false;
+            followTimer = 0f;
+            shouldMove = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 碰撞到玩家时停止移动
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            Destroy(gameObject);
+
+        }
+        else if (collision.gameObject.CompareTag("Door"))
+        {
+            transform.position = initialPosition;
+
+        }
+    }
+}
